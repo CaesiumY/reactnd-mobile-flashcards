17ae3a8a7f93497f07a6c26db45fde1209e8dcd6
caesiumy <mn0658@naver.com>
2020-06-08 22:19:18 +0900

    feat: Add utils basic frame

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+const getDecks = () => {
+  // TODO - return all decks
+};
+
+const getDeck = (id) => {
+  // TODO - return a single deck which has certain id
+};
+
+const saveDeckTitle = (title) => {
+  // TODO - add title to the decks object
+};
+
+const addCardToDeck = (title, card) => {
+  // TODO - add the card object to the deck of title
+};
