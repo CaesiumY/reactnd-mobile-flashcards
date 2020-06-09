54b63be86cd49ed9386a8fe1b905a486a7648384
caesiumy <mn0658@naver.com>
2020-06-09 22:14:39 +0900

    feat: Add InputLayout component

---
+++ components/InputLayout.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, TextInput } from "react-native";
+import { tintColor } from "../constants/Colors";
+
+const InputLayout = ({ placeholder, onChangeText, value }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      onChangeText={onChangeText}
+      value={value}
+    ></TextInput>
+  );
+};
+
+export default InputLayout;
+
+const styles = StyleSheet.create({
+  input: {
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: "white",
+    height: 40,
+    borderRadius: 2,
+    padding: 10,
+    borderColor: tintColor,
+    borderWidth: 2,
+  },
+});
