7f652cbd8204e3164420e54a0914a119061f812a
caesiumy <mn0658@naver.com>
2020-06-09 22:14:17 +0900

    feat: Add TextButton component

---
+++ components/TextButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { tintColor } from "../constants/Colors";
+
+const TextButton = ({ children }) => {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: tintColor,
+    padding: 10,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 23,
+    borderRadius: 5,
+  },
+});
