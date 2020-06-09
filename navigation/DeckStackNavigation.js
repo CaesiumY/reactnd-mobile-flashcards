cb7da2701768c4d7e04fd1890d50f5de93b0a7dd
caesiumy <mn0658@naver.com>
2020-06-10 02:07:38 +0900

    chore: Add redux related packages

--- navigation/DeckStackNavigation.js
+++ navigation/DeckStackNavigation.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { View, Text } from "react-native";
 
 const DeckStack = createStackNavigator();
 
