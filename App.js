d3de8d5f4c9fbb3f5dc828aa47bcbd3e7a61660f
caesiumy <mn0658@naver.com>
2020-06-10 23:33:16 +0900

    feat: Add store Provider

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import useCachedResources from "./hooks/useCachedResources";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import LinkingConfiguration from "./navigation/LinkingConfiguration";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
+
+const store = createStore(reducer);
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
@@ -13,12 +18,14 @@ export default function App(props) {
     return null;
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <BottomTabNavigator />
-        </NavigationContainer>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
+          <NavigationContainer linking={LinkingConfiguration}>
+            <BottomTabNavigator />
+          </NavigationContainer>
+        </View>
+      </Provider>
     );
   }
 }
