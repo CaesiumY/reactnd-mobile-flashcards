cbbf8af7a370ce3e84ce046aaa994a1ca4d829bb
caesiumy <mn0658@naver.com>
2020-06-16 20:42:51 +0900

    feat: Add cardContent in ResultScreen

--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -1,20 +1,138 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Animated } from "react-native";
 import { connect } from "react-redux";
-// import * as Progress from "react-native-progress";
+import * as Progress from "react-native-progress";
+import { tintColor } from "../constants/Colors";
 
 export class ResultScreen extends Component {
+  state = {
+    resultText: "",
+    scoreToPercent: 0,
+    bounceValue: new Animated.Value(1),
+    opacity: new Animated.Value(0),
+    showContents: false,
+  };
+
+  componentDidMount() {
+    const { score, questionCount } = this.props.route.params;
+    const { bounceValue, opacity } = this.state;
+    this.setState(
+      {
+        scoreToPercent: score / questionCount,
+      },
+      () => {
+        this.state.scoreToPercent === 1
+          ? this.setState({ resultText: "Excellent!" })
+          : this.setState({ resultText: "Cheer Up!" });
+      }
+    );
+
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.sequence([
+      Animated.timing(bounceValue, { toValue: 1.05, duration: 400 }),
+      Animated.spring(bounceValue, { toValue: 1, friction: 4 }),
+    ]).start(() => {
+      this.setState({ showContents: true });
+    });
+  }
+
   render() {
     const { title, score, questionCount } = this.props.route.params;
+    const {
+      scoreToPercent,
+      resultText,
+      bounceValue,
+      showContents,
+    } = this.state;
     return (
-      <View>
-        <Text> {title} </Text>
-        <Text> {score} </Text>
-        <Text> {questionCount} </Text>
+      <View style={styles.container}>
+        <Animated.View
+          style={[styles.card, { transform: [{ scale: bounceValue }] }]}
+        >
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardHeaderText}>Your Score is...</Text>
+          </View>
+
+          <View style={styles.cardContent}>
+            {showContents && (
+              <>
+                <View style={styles.progressContainer}>
+                  <Progress.Circle
+                    size={160}
+                    formatText={() => `${scoreToPercent * 100}%`}
+                    animated={true}
+                    progress={scoreToPercent}
+                    showsText={true}
+                    color={tintColor}
+                    borderWidth={5}
+                    thickness={8}
+                  />
+                </View>
+                <Text style={styles.cardContentText}>
+                  {score} / {questionCount}
+                </Text>
+                <Text style={styles.resultText}>{resultText}</Text>
+              </>
+            )}
+          </View>
+        </Animated.View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 20,
+  },
+  card: {
+    backgroundColor: "white",
+    height: "100%",
+    borderRadius: 20,
+    justifyContent: "space-between",
+  },
+
+  cardHeader: {
+    flex: 0.15,
+    justifyContent: "center",
+    backgroundColor: tintColor,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  cardHeaderText: {
+    color: "white",
+    fontWeight: "bold",
+    padding: 5,
+    fontSize: 24,
+    textAlign: "center",
+  },
+  cardContent: {
+    flex: 1,
+    padding: 20,
+  },
+  cardContentText: {
+    fontSize: 35,
+    textAlign: "center",
+    padding: 20,
+  },
+  progressContainer: {
+    alignItems: "center",
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 35,
+    fontWeight: "bold",
+    color: tintColor,
+  },
+});
 
 const mapStateToProps = (state) => {
   return {};
