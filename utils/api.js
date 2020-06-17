070d683e2137b4c5ef15c98303b58a091e497ed0
caesiumy <mn0658@naver.com>
2020-06-17 23:17:15 +0900

    feat: Add api content

--- utils/api.js
+++ utils/api.js
@@ -1,15 +1,58 @@
-const getDecks = () => {
+import AsyncStorage from "@react-native-community/async-storage";
+
+const DATABASE_KEY = "flash:test";
+
+export const getDecks = () => {
   // TODO - return all decks
+  return AsyncStorage.getItem(DATABASE_KEY).then((result) =>
+    console.log(result)
+  );
 };
 
-const getDeck = (id) => {
+export const getDeck = (id) => {
   // TODO - return a single deck which has certain id
+  return AsyncStorage.getItem(DATABASE_KEY).then((result) => {
+    const parsed = JSON.parse(result);
+    console.log(parsed);
+  });
 };
 
-const saveDeckTitle = (title) => {
+export const saveDeckTitle = (title) => {
   // TODO - add title to the decks object
+  return AsyncStorage.mergeItem(
+    DATABASE_KEY,
+    JSON.stringify({
+      [title]: {
+        title: title,
+        questions: [],
+      },
+    })
+  );
 };
 
-const addCardToDeck = (title, card) => {
+export const addCardToDeck = (title, question) => {
   // TODO - add the card object to the deck of title
+  return AsyncStorage.mergeItem(
+    DATABASE_KEY,
+    JSON.stringify({
+      [title]: {
+        ...state[title],
+        questions: [
+          ...state[title].questions,
+          {
+            question: question.question,
+            answer: question.answer,
+          },
+        ],
+      },
+    })
+  );
+};
+
+export const removeDeck = (title) => {
+  return AsyncStorage.getItem(DATABASE_KEY).then((results) => {
+    const parsed = JSON.parse(results);
+    delete results[title];
+    AsyncStorage.setItem(DATABASE_KEY, JSON.stringify(results));
+  });
 };
